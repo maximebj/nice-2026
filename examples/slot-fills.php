f13c59d28937e532aabc9649741a8434d788e5b0
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2026-02-15 11:24:47 +0100

    Enregistrement des meta du slotfill

--- examples/slot-fills.php
+++ examples/slot-fills.php
@@ -33,3 +33,39 @@ function nice_2026_enqueue_admin_slot_fills_assets(): void
 	);
 }
 add_action('enqueue_block_editor_assets', 'nice_2026_enqueue_admin_slot_fills_assets');
+
+# Déclaration des meta données pour les slotFills
+function nice_2026_register_slot_fills_meta(): void
+{
+	$meta_fields = [
+		'nice_2026_conference_name' => [
+			'type'    => 'string',
+			'default' => '',
+		],
+		'nice_2026_conference_confirmed' => [
+			'type'    => 'boolean',
+			'default' => false,
+		],
+		'nice_2026_event_day' => [
+			'type'    => 'string',
+			'default' => '',
+		],
+		'nice_2026_conference_color' => [
+			'type'    => 'string',
+			'default' => '',
+		],
+	];
+
+	foreach ($meta_fields as $meta_key => $args) {
+		register_post_meta('', $meta_key, [
+			'show_in_rest'  => true,
+			'single'        => true,
+			'type'          => $args['type'],
+			'default'       => $args['default'],
+			'auth_callback' => function () {
+				return current_user_can('edit_posts');
+			},
+		]);
+	}
+}
+add_action('init', 'nice_2026_register_slot_fills_meta');
--- src/slotFills/index.js
+++ src/slotFills/index.js
@@ -1,19 +1,37 @@
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
-import { PluginSidebar, } from '@wordpress/editor';
+import { PluginSidebar } from '@wordpress/editor';
 import { useSettings } from '@wordpress/block-editor';
-import { ColorPalette, PanelBody, TextControl, ToggleControl, __experimentalVStack as VStack } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import {
+  ColorPalette,
+  PanelBody,
+  TextControl,
+  ToggleControl,
+  __experimentalVStack as VStack,
+  __experimentalToggleGroupControl as ToggleGroupControl,
+  __experimentalToggleGroupControlOption as ToggleGroupControlOption,
+} from '@wordpress/components';
 import { starFilled } from '@wordpress/icons';
-import { useState } from '@wordpress/element';
 
 import './style.scss';
 
 function PluginWcNicePanel() {
-  const [name, setName] = useState('Conférence Nice 2026');
-  const [color, setColor] = useState('#000000');
-  const [isLive, setIsLive] = useState(true);
 
-  const [themeColors] = useSettings("color.palette.theme"); // Récupérer les couleurs du thème
+  const [themeColors] = useSettings("color.palette.theme");
+
+  // Lire les meta depuis le store de l'éditeur
+  const meta = useSelect((select) => {
+    return select('core/editor').getEditedPostAttribute('meta') || {};
+  }, []);
+
+  // Dispatcher pour modifier les meta via editPost()
+  const { editPost } = useDispatch('core/editor');
+
+  // Fonction utilitaire pour mettre à jour une meta
+  const updateMeta = (key, value) => {
+    editPost({ meta: { [key]: value } });
+  };
 
   return (
     <PluginSidebar title={__("Bonjour Nice !", "nice-2026")} icon={starFilled}>
@@ -22,20 +40,35 @@ function PluginWcNicePanel() {
           <TextControl
             label={__("Nom de la conférence", "nice-2026")}
             help={__("Entrez le nom de la conférence", "nice-2026")}
-            value={name}
-            onChange={(value) => setName(value)}
+            value={meta.nice_2026_conference_name || ''}
+            onChange={(value) => updateMeta('nice_2026_conference_name', value)}
           />
+
           <ToggleControl
             label={__("Conférence confirmée ?", "nice-2026")}
             help={__("Est-ce que la conférence est confirmée ?", "nice-2026")}
-            checked={isLive}
-            onChange={(value) => setIsLive(value)}
+            checked={meta.nice_2026_conference_confirmed || false}
+            onChange={(value) => updateMeta('nice_2026_conference_confirmed', value)}
           />
+
+          <ToggleGroupControl
+            label={__('Journée de l\'événement', 'nice-2026')}
+            value={meta.nice_2026_event_day || ''}
+            onChange={(value) => updateMeta('nice_2026_event_day', value)}
+            isBlock
+            __next40pxDefaultSize
+            __nextHasNoMarginBottom
+          >
+            <ToggleGroupControlOption key="jeudi" value="jeudi" label={__('Jeudi', 'nice-2026')} />
+            <ToggleGroupControlOption key="vendredi" value="vendredi" label={__('Vendredi', 'nice-2026')} />
+            <ToggleGroupControlOption key="samedi" value="samedi" label={__('Samedi', 'nice-2026')} />
+          </ToggleGroupControl>
+
           <ColorPalette
             label={__("Couleur de la conférence", "nice-2026")}
             help={__("Choisissez la couleur de la conférence", "nice-2026")}
-            value={color}
-            onChange={(value) => setColor(value)}
+            value={meta.nice_2026_conference_color || ''}
+            onChange={(value) => updateMeta('nice_2026_conference_color', value)}
             colors={themeColors}
           />
         </VStack>
